715813c79d88b53fdffc60d5159aa724d10bfa82
Anastasiia Romanenko <druzhkoshowewed@gmail.com>
2021-03-01 17:59:31 +0300

    task 4

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import os
+import pygame
 import sys
 
-import pygame
-
+FPS = 50
+player = None
+fl = True
+clock = pygame.time.Clock()
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
-FPS = 50
-clock = pygame.time.Clock()
+xxx, yyy = 0, 0
 
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
+    try:
+        image = pygame.image.load(fullname).convert()
+    except SystemError:
         print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
 def terminate():
     pygame.quit()
     sys.exit()
+
 
 def start_screen():
     intro_text = []
@@ -65,18 +77,12 @@ def load_level(filename):
 
 
 tile_images = {
-        'wall': load_image('box.png'),
-        'empty': load_image('grass.png')
-    }
-player_image = load_image('mario.png')
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png', -1)
 
 tile_width = tile_height = 50
-
-player = None
-
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
 
 
 class Tile(pygame.sprite.Sprite):
@@ -86,26 +92,7 @@ class Tile(pygame.sprite.Sprite):
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
-    
-    def move(self, left, right, up, down, level_x, level_y):
-        if left:
-            self.rect.x -= tile_width
-            if self.rect.x < 0:
-                self.rect.x += tile_width * (level_x - 1)
-                print('moved')
-        if right:
-            self.rect.x += tile_width
-            if self.rect.x > tile_width * (level_x):
-                self.rect.x = 0
-        if up:
-            self.rect.y -= tile_height
-            if self.rect.y < 0:
-                self.rect.y += tile_height * (level_y - 1)
-        if down:
-            self.rect.y += tile_height
-            if self.rect.y > tile_height * (level_y):
-                self.rect.y = 0
-    
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
@@ -113,80 +100,109 @@ class Player(pygame.sprite.Sprite):
         self.image = player_image
         self.rect = self.image.get_rect().move(
             tile_width * pos_x + 15, tile_height * pos_y + 5)
-    
-    def can_move(self, left, right, up, down):
-        res = False
+
+    def move(self, left, right, up, down):
         if left:
-            self.rect.x += tile_width
-            if pygame.sprite.spritecollideany(self, tiles_group).tile_type != 'wall':
-                res = True
+            save = self.rect.x
             self.rect.x -= tile_width
+            if pygame.sprite.spritecollideany(self, tiles_group).tile_type == 'wall':
+                self.rect.x = save
+                return False
+            self.rect.x = save
         if right:
-            self.rect.x -= tile_width
-            if pygame.sprite.spritecollideany(self, tiles_group).tile_type != 'wall':
-                res = True
+            save = self.rect.x
             self.rect.x += tile_width
+            if pygame.sprite.spritecollideany(self, tiles_group).tile_type == 'wall':
+                self.rect.x = save
+                return False
+            self.rect.x = save
         if up:
-            self.rect.y += tile_height
-            if pygame.sprite.spritecollideany(self, tiles_group).tile_type != 'wall':
-                res = True
-            self.rect.y -= tile_width
-        if down:
+            save = self.rect.y
             self.rect.y -= tile_height
-            if pygame.sprite.spritecollideany(self, tiles_group).tile_type != 'wall':
-                res = True
-            self.rect.y += tile_width
-        
-        return res
+            if pygame.sprite.spritecollideany(self, tiles_group).tile_type == 'wall':
+                self.rect.y = save
+                return False
+            self.rect.y = save
+        if down:
+            save = self.rect.y
+            self.rect.y += tile_height
+            if pygame.sprite.spritecollideany(self, tiles_group).tile_type == 'wall':
+                self.rect.y = save
+                return False
+            self.rect.y = save
+        return True
+
 
 def generate_level(level):
+    global fl, xxx, yyy, all_sprites, tiles_group, player_group
+    all_sprites = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
     new_player, x, y = None, None, None
-    # level_map = [[None for i in range(len(level[j]))] for j in range(level)]
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
                 Tile('empty', x, y)
             elif level[y][x] == '#':
                 Tile('wall', x, y)
-            elif level[y][x] == '@':
+            elif level[y][x] == '@' and fl:
                 Tile('empty', x, y)
                 new_player = Player(x, y)
+                xxx, yyy = x, y
+                fl = False
+            elif not fl and level[y][x] == '@':
+                Tile('empty', x, y)
+    if not fl:
+        new_player = Player(xxx, yyy)
     # вернем игрока, а также размер поля в клетках            
     return new_player, x, y
 
 
-def move(left, right, up, down, level_x, level_y):
-    for tile in tiles_group:
-        tile.move(left, right, up, down, level_x, level_y)
-
-
 def game():
-    player, level_x, level_y = generate_level(load_level("level_1.txt"))
+    file = load_level('level_1.txt')
+    player, level_x, level_y = generate_level(file)
+    spis = []
 
     start_screen()
 
     while True:
+        spis = []
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    if player.can_move(1, 0, 0, 0):
-                        print('can_move')
-                        move(1, 0, 0, 0, level_x, level_y)
+                    if player.move(1, 0, 0, 0):
+                        for i in range(len(file)):
+                            ppp = ''
+                            ppp += file[i][-1]
+                            ppp += file[i][:-1]
+                            spis.append(ppp)
+                        file = spis[::]
+                        player, level_x, level_y = generate_level(file)
                 elif event.key == pygame.K_UP:
-                    if player.can_move(0, 0, 1, 0):
-                        print('can_move')
-                        move(0, 0, 1, 0, level_x, level_y)
+                    if player.move(0, 0, 1, 0):
+                        spis.append(file[-1])
+                        spis.extend(file[:-1])
+                        file = spis[::]
+                        player, level_x, level_y = generate_level(file)
                 elif event.key == pygame.K_DOWN:
-                    if player.can_move(0, 0, 0, 1):
-                        move(0, 0, 0, 1, level_x, level_y)
+                    if player.move(0, 0, 0, 1):
+                        spis = file[1:]
+                        spis.append(file[0])
+                        file = spis[::]
+                        player, level_x, level_y = generate_level(file)
                 elif event.key == pygame.K_RIGHT:
-                    if player.can_move(0, 1, 0, 0):
-                        move(0, 1, 0, 0, level_x, level_y)
+                    if player.move(0, 1, 0, 0):
+                        for i in range(len(file)):
+                            ppp = ''
+                            ppp += file[i][1:]
+                            ppp += file[i][0]
+                            spis.append(ppp)
+                        file = spis[::]
+                        player, level_x, level_y = generate_level(file)
 
         pygame.display.flip()
-        screen.fill((0, 0, 0))
         all_sprites.draw(screen)
         player_group.draw(screen)
         clock.tick(FPS)
